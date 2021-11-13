29f3f5e71ed1a032a0afefc55582a66c6e52daf9
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2021-11-13 18:37:28 +0700

    First Commit

---
+++ routers/route.js
@@ -0,0 +1,284 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var urlencodeParser = bodyParser.urlencoded({ extended: false });
+var validator = require('express-validator');
+
+module.exports = function (app) {
+
+      function isUserAllowed(req, res, next) {
+            sess = req.session;
+            if (sess.user) {
+                  return next();
+            }
+            else { res.redirect('/login'); }
+      }
+
+      app.get('/', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Dashboard' };
+            res.render('Dashboard/index');
+      });
+
+      app.get('/dashboard-2', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Dashboard Two' };
+            res.render('Dashboard/dashboard-2');
+      });
+
+      // Store
+      app.get('/products', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Products' };
+            res.render('Store/products');
+      });
+      app.get('/product-detail', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Product Detail' };
+            res.render('Store/product-detail');
+      });
+      app.get('/add-product', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Add Product' };
+            res.render('Store/add-product');
+      });
+      app.get('/categories', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Categories' };
+            res.render('Store/categories');
+      });
+      app.get('/add-category', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Add Category' };
+            res.render('Store/add-category');
+      });
+
+      //Customers
+      app.get('/orders', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Orders' };
+            res.render('Customers/orders');
+      });
+      app.get('/customers', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Customers' };
+            res.render('Customers/customers');
+      });
+      app.get('/cart', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Cart' };
+            res.render('Customers/cart');
+      });
+      app.get('/checkout', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Checkout' };
+            res.render('Customers/checkout');
+      });
+
+
+      // Invoice
+      app.get('/invoices-list', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Invoice List' };
+            res.render('Invoice/invoices-list');
+      });
+      app.get('/invoices-detail', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Invoice Detail' };
+            res.render('Invoice/invoices-detail');
+      });
+
+      // Contacts
+      app.get('/contacts-list', isUserAllowed, function (req, res) {
+            res.locals = { title: 'User List' };
+            res.render('Contacts/contacts-list');
+      });
+      app.get('/contacts-profile', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Profile' };
+            res.render('Contacts/contacts-profile');
+      });
+
+      // UI
+      app.get('/ui-alerts', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Alerts' };
+            res.render('Ui/ui-alerts');
+      });
+      app.get('/ui-buttons', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Buttons' };
+            res.render('Ui/ui-buttons');
+      });
+      app.get('/ui-cards', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Cards' };
+            res.render('Ui/ui-cards');
+      });
+      app.get('/ui-carousel', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Carousel' };
+            res.render('Ui/ui-carousel');
+      });
+      app.get('/ui-dropdowns', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Dropdowns' };
+            res.render('Ui/ui-dropdowns');
+      });
+      app.get('/ui-grid', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Grid' };
+            res.render('Ui/ui-grid');
+      });
+      app.get('/ui-images', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Images' };
+            res.render('Ui/ui-images');
+      });
+      app.get('/ui-lightbox', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Lightbox' };
+            res.render('Ui/ui-lightbox');
+      });
+      app.get('/ui-modals', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Modals' };
+            res.render('Ui/ui-modals');
+      });
+      app.get('/ui-rangeslider', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Range Slider' };
+            res.render('Ui/ui-rangeslider');
+      });
+      app.get('/ui-session-timeout', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Session Timeout' };
+            res.render('Ui/ui-session-timeout');
+      });
+      app.get('/ui-progressbars', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Progress Bars' };
+            res.render('Ui/ui-progressbars');
+      });
+      app.get('/ui-sweet-alert', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Sweet Alert' };
+            res.render('Ui/ui-sweet-alert');
+      });
+      app.get('/ui-tabs-accordions', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Tabs & Accordions' };
+            res.render('Ui/ui-tabs-accordions');
+      });
+      app.get('/ui-typography', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Typography' };
+            res.render('Ui/ui-typography');
+      });
+      app.get('/ui-video', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Video' };
+            res.render('Ui/ui-video');
+      });
+      app.get('/ui-general', isUserAllowed, function (req, res) {
+            res.locals = { title: 'General' };
+            res.render('Ui/ui-general');
+      });
+      app.get('/ui-colors', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Colors' };
+            res.render('Ui/ui-colors');
+      });
+      app.get('/ui-rating', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Rating' };
+            res.render('Ui/ui-rating');
+      });
+      app.get('/ui-notifications', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Notifications' };
+            res.render('Ui/ui-notifications');
+      });
+     
+
+      // Forms
+      app.get('/form-elements', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Basic Elements' };
+            res.render('Form/form-elements');
+      });
+      app.get('/form-validation', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Validation' };
+            res.render('Form/form-validation');
+      });
+      app.get('/form-advanced', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Advanced Plugins' };
+            res.render('Form/form-advanced');
+      });
+      app.get('/form-editors', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Editors' };
+            res.render('Form/form-editors');
+      });
+      app.get('/form-uploads', isUserAllowed, function (req, res) {
+            res.locals = { title: 'File Uploads' };
+            res.render('Form/form-uploads');
+      });
+      app.get('/form-xeditable', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Xeditable' };
+            res.render('Form/form-xeditable');
+      });
+      app.get('/form-repeater', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Repeater' };
+            res.render('Form/form-repeater');
+      });
+      app.get('/form-wizard', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Wizard' };
+            res.render('Form/form-wizard');
+      });
+      app.get('/form-mask', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Form Mask' };
+            res.render('Form/form-mask');
+      });
+     
+      // Tables
+      app.get('/tables-basic', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Bootstrap Basic' };
+            res.render('Tables/tables-basic');
+      });
+      app.get('/tables-datatable', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Datatables' };
+            res.render('Tables/tables-datatable');
+      });
+      app.get('/tables-responsive', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Responsive' };
+            res.render('Tables/tables-responsive');
+      });
+      app.get('/tables-editable', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Editable' };
+            res.render('Tables/tables-editable');
+      });
+      
+      // Charts
+      app.get('/charts-apex', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Apex' };
+            res.render('Charts/charts-apex');
+      });
+      app.get('/charts-chartjs', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Chartjs' };
+            res.render('Charts/charts-chartjs');
+      });
+      app.get('/charts-flot', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Flot' };
+            res.render('Charts/charts-flot');
+      });
+      app.get('/charts-knob', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Jquery Knob' };
+            res.render('Charts/charts-knob');
+      });
+      app.get('/charts-sparkline', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Sparkline' };
+            res.render('Charts/charts-sparkline');
+      });
+
+      // Icons
+      app.get('/icons-unicons', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Unicons' };
+            res.render('Icons/icons-unicons');
+      });
+      app.get('/icons-boxicons', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Boxicons' };
+            res.render('Icons/icons-boxicons');
+      });
+      app.get('/icons-materialdesign', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Material Design' };
+            res.render('Icons/icons-materialdesign');
+      });
+      app.get('/icons-dripicons', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Dripicons' };
+            res.render('Icons/icons-dripicons');
+      });
+      app.get('/icons-fontawesome', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Font Awesome' };
+            res.render('Icons/icons-fontawesome');
+      });
+
+      // Maps
+      app.get('/maps-google', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Google' };
+            res.render('Maps/maps-google');
+      });
+      app.get('/maps-vector', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Vector' };
+            res.render('Maps/maps-vector');
+      });
+      app.get('/maps-leaflet', isUserAllowed, function (req, res) {
+            res.locals = { title: 'Leaflet' };
+            res.render('Maps/maps-leaflet');
+      });
+      
+}
